adc55f1fec36547c4e0608d74ea2dc3f619f8a12
André Gonçalves <andregsistemas@outlook.com>
2021-09-27 13:59:02 -0300

    Atualização 0.0.1 do zeldaminiclone

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 Upload dos arquivos Java para o repositório GitHub
 
 Códicos da entrada de interface adicionado na classe  Game.java
+
+27/09/2021
+
+Atualização do código fonte da classe Game, onde foi adicionado métodos para exibir um retângulo vermelho em um fundo preto dentro interface grafica do JAVA.
+
+Para mais info, ver comentários do código fonte.
--- src/zeldaminiclone/Game.java
+++ src/zeldaminiclone/Game.java
@@ -1,7 +1,10 @@
 package zeldaminiclone;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -13,6 +16,28 @@ public class Game extends Canvas implements Runnable{
 		this.setPreferredSize(new Dimension( WIDTH, HEIGHT ));
 		
 	}
+	
+	public void tick() { //Método responsável pela lógica do jogo, movimentação, colisões, etc.
+		
+	} 
+	
+	public void render() { // método para realizar a renderização do jogo.
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black); // Método para setar a cor
+		g.fillRect(0, 0, WIDTH, HEIGHT); //metodo para exibir um retângulo na tela
+		
+		g.setColor(Color.red);
+		g.fillRect(0, 0, 50, 50);
+		
+		bs.show(); //Exibe na interface o retângulo
+	} 
 	
 	public static void main (String[] args) {
 		Game game = new Game();
@@ -36,8 +61,13 @@ public class Game extends Canvas implements Runnable{
 	public void run() {
 		
 		while(true) {
-			
-			System.out.println("Chamando game looping!");
+			tick();
+			render();
+			try {
+				Thread.sleep(1000/60); //rodar o jogo a 60fps
+			} catch (InterruptedException e) {
+				e.printStackTrace(); //rotina de erro
+			}
 			
 		}
 		
